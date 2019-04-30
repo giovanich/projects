bf9cc944aa9e6401fbd6be8788c66a8bc073d316
giovanich <50132180+giovanich@users.noreply.github.com>
2019-04-30 16:07:31 +0700

    Update Crawling_Merchant_shopee.py

--- Crawling_Merchant_shopee.py
+++ Crawling_Merchant_shopee.py
@@ -1,3 +1,4 @@
+
 from SQLite import DB
 from OmniCrawler import Selenium
 from datetime import datetime
@@ -22,35 +23,32 @@ class Run:
     Search = "beauty"
 
     def Crawling(self):
-        self.Selenium.link = "https://shopee.co.id/search?keyword="+Concatenate().InfuseSeparator(main=self.Search, separator=self.word_separator)+"&page="+str(self.product_page)
+        self.Selenium.link = "https://shopee.co.id/Kecantikan-cat.14840"+Concatenate().InfuseSeparator(main=self.Search, separator=self.word_separator)+"?page="+str(self.product_page)
+        #"https://shopee.co.id/search?keyword="+Concatenate().InfuseSeparator(main=self.Search, separator=self.word_separator)+"&page="+str(self.product_page)
         self.Selenium.Load(self.Selenium.link)
-        print(self.Selenium.link);input()
+        time.sleep(3)
+        print(self.Selenium.link);#input()
         while self.product_page < self.max_product_page:
             item_count = 1
             item_ordinal = self.Selenium.ExtractElements("//*[@id='main']/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[2]/div")
-            print(len(item_ordinal));input()
+            print(len(item_ordinal));#input()
             self.product_page += 1
             while item_count <= len(item_ordinal):
                 sub_root = "//*[@id='main']/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[2]/div[",str(item_count),"]"
                 self.Product_location = self.Selenium.ExtractElementText("".join(["//*[@id='main']/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[2]/div[",str(item_count),"]/div/a/div/div[2]/div[5]"]))
                 print(self.Product_location)
-                self.Product_price = int(self.Selenium.ExtractElementText("".join(["//*[@id='main']/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[2]/div[",str(item_count),"]/div/a/div/div[2]/div[2]/div[1]/span[2]"])))
+                self.Product_price = self.Selenium.ExtractElementText("".join(["//*[@id='main']/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[2]/div[",str(item_count),"]/div/a/div/div[2]/div[2]"])).replace('Rp', '')
+                #self.Product_price = int(self.Selenium.ExtractElementText("".join(["//*[@id='main']/div/div[2]/div[2]/div/div/div[4]/div[2]/div/div/div[2]/div[",str(item_count),"]/div/a/div/div[2]/div[2]/div[1]"])))
                 print(self.Product_price)
-                self.Product_price_dis = int(self.Selenium.ExtractElementText("".join([sub_root,'/div/a/div/div[2]/div[2]/div[2]/span[2]'])))
-                print(self.Product_price_dis)
-                self.Product_name = self.Selenium.ExtractElementText("".join(["//*[@id='main']/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[2]/div[",str(item_count),"]/div/a/div/div[2]/div[1]/div"]))
-                print(self.Product_name)
-                check_area = self.Selenium.ExtractElements("".join([sub_root,'/div/a/div/div[2]/div[4]/div']))
-                print(check_area);input()
-                if check_area is 0 or check_area is None:
-                    self.Product_sold = 0
-                    print(self.Product_sold)
-                else:
-                    self.Product_sold = int(self.Selenium.ExtractElementText("".join([sub_root,'/div/a/div/div[2]/div[4]/div[3]'])).replace(' Terjual/ Bulan','').replace('.',''))
-                    print(self.Product_sold)
-                input()
+                #self.Product_price_dis = int(self.Selenium.ExtractElementText("".join([sub_root,'/div/a/div/div[2]/div[2]/div[2]/span[2]'])))
+                #print(self.Product_price_dis)
+                self.Product_name = self.Selenium.ExtractElementAttribute('href',"".join(["//*[@id='main']/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[2]/div[",str(item_count),"]/div/a"])).replace("https://shopee.co.id/",'').replace('-',' ')
+                index = self.Product_name.split('i.')
+                print(index[0])
+                item_count +=1
 
             self.Selenium.link = "https://shopee.co.id/search?keyword="+Concatenate().InfuseSeparator(main=self.Search, separator=self.word_separator)+"&page="+str(self.product_page)
             self.Selenium.Load(self.Selenium.link)
+            time.sleep(3)
 
 Run().Crawling()
